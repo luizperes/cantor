8a9e4cd52247ffbb70362e363f14a0ed12277eba
Luiz Fernando Peres <luiz_peres10@hotmail.com>
2018-01-14 14:05:32 -0800

    Small changes on the grammar

--- src/grammar.hs
+++ src/grammar.hs
@@ -5,45 +5,58 @@ import System.Environment
 data Constant = StringLit [Char]
               | FloatLit Float
               | IntLit   Int
+              deriving Show
 
 data Identifier = IId String
+                deriving Show
 
 data BindingName = BId Identifier
+                 deriving Show
 
 data Type = N
           | Z
           | R
           | Universe
           | Custom Identifier
+          deriving Show
 
 data PatternStmt = ForAllStmt BindingName Type
                  | ThereExistsStmt BindingName Type
                  | BindingStmt BindingName
+                 deriving Show
 
 data Factor = FConst  Constant
             | FBind   BindingName Factor
             | FParens Factor
+            deriving Show
 
 data AddOp = Add
            | Sub
+           deriving Show
 
 data MulOp = Mul
            | Div
            | Mod
            | Exp
+           deriving Show
 
 data Term = TFactor Factor
           | TMFactor Factor MulOp Factor
+          deriving Show
 
 data Expression = ETerm Term
                 | EMTerm Term AddOp Expression
+                deriving Show
 
 data Binding = BBind BindingName PatternStmt [Expression]
+             deriving Show
 
 data BeginStmt = LetStmt [Binding]
                | DoStmt  Expression
+               deriving Show
 
-data Grammar = GConstant Constant
+data Grammar = Epsilon
+             | GConstant Constant
              | GIdentifier Identifier
              | GBindingName BindingName
              | GType Type
@@ -55,4 +68,6 @@ data Grammar = GConstant Constant
              | GExpression Expression
              | GBinding Binding
              | GBeginStmt BeginStmt
+             | GProgram [BeginStmt]
+             deriving Show
 
