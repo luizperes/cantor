9c62a1f4fadd94447da65e2bdc6ab341c3e1a37d
Luiz Fernando Peres <luiz_peres10@hotmail.com>
2018-02-15 21:47:48 -0800

    Starting to apply functions on the interpreter

--- src/Interpreter.hs
+++ src/Interpreter.hs
@@ -3,12 +3,21 @@ module Interpreter where
 import System.Environment
 import Grammar
 
-exec' :: Program -> String
-exec' (Prog stmts) = interpret' (filter isDoStmt stmts) (filter (\x -> not (isDoStmt x)) stmts)
+exec' :: Program -> [Constant]
+exec' (Prog stmts) =
+  interpret'
+    (filter isDoStmt stmts)
+    (filter (\x -> not (isDoStmt x)) stmts)
 
 isDoStmt :: BeginStmt -> Bool
 isDoStmt (DoStmt _) = True
 isDoStmt _ = False
 
-interpret' :: [BeginStmt] -> [BeginStmt] -> String
-interpret' doStmts letStmts = show doStmts
+interpret' :: [BeginStmt] -> [BeginStmt] -> [Constant]
+interpret' doStmts letStmts =
+  (map
+    (\doStmt -> interpretDo' doStmt letStmts)
+    doStmts)
+
+interpretDo' :: BeginStmt -> [BeginStmt] -> Constant
+interpretDo' doStmt letStmts = SetLit [IntLit 1]
